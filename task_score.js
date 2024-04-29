362403c045efecdcd712383595ca49f93f8791fa
turavas <olapola971@gmail.com>
2024-04-29 20:17:42 +0200

    HW_2.1.3/Added score file

---
+++ task_score.js
@@ -0,0 +1,12 @@
+let introScore = 5;
+let gitScore = 15;
+let jsScore = 10;
+
+let avgScore = (introScore + gitScore + jsScore)/3;
+
+//console.log(`Средний балл по трем модулям равен ${avgScore}`);
+
+let studentName = "Anna";
+let studentSirname = "Poup";
+
+console.log(`${studentName} ${studentSirname}: Средний балл по трем модулям равен ${avgScore}`);
