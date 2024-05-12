ad2bca8afb0207b870611d6a44a5ee0b2c62797a
turavas <olapola971@gmail.com>
2024-05-12 13:19:30 +0200

    HW_2.3.1.2/Added test data

--- players.js
+++ players.js
@@ -1,30 +1,28 @@
-/* Задача: 
-выведите в консоль игрока с максимальным значением scorePoints. 
-Используйте цикл, обращение к свойству через точку и метод глобального объекта Math, 
-позволяющий найти большее число из всех возможных.
-*/
-
-// Тестовые данные - массив объектов players 
 const players = [
     {
       id: 1,
-      name: "Ivan",
-      scorePoints: 4500,
+      name: "Lora",
+      scorePoints: 380,
     },
     {
       id: 2,
       name: "Petr",
-      scorePoints: 3600,
+      scorePoints: 2,
     },
     {
       id: 3,
-      name: "Vadim",
-      scorePoints: 3433,
+      name: "Mona",
+      scorePoints: 13,
     },
     {
       id: 4,
-      name: "Olga",
+      name: "Poul",
       scorePoints: 2356,
+    },
+    {
+      id: 5,
+      name: "Dima",
+      scorePoints: 2357,
     },
   ];
   
